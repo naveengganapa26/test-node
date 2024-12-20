b409a40ff5dc551b3bacf7e9584fa7578d38f06b
naveengganapa <naveenise2022@gmail.com>
2024-12-20 11:36:40 +0530

    port changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const restaurantRoutes = require('./routes/restaurantRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors());
